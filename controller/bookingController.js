76abbb12413c5984cb58f905931a02cf0e97ced5
bharath1234567 <bharathcs2013@gmail.com>
2023-03-11 23:38:43 +0530

    added stripe webhook

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -12,7 +12,9 @@ exports.getCheckoutSession=catchAsync(async(req,res,next)=>{
     const session = await stripe.checkout.sessions.create({
         payment_method_types:['card'],
         mode:'payment',
-        success_url:`${req.protocol}://${req.get("host")}/my-tours/?tour=${tour._id}&user=${req.user.id}&price=${tour.price}`,
+        success_url:`${req.protocol}://${req.get("host")}/my-tours`,
+        
+        // success_url:`${req.protocol}://${req.get("host")}/my-tours/?tour=${tour._id}&user=${req.user.id}&price=${tour.price}`,
         cancel_url:`${req.protocol}://${req.get("host")}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id:req.params.tourId,
@@ -27,7 +29,7 @@ exports.getCheckoutSession=catchAsync(async(req,res,next)=>{
                         description: tour.summary,
                         name:`${tour.name} Tour`,
 
-                        images:[`https://www.natours.dev/img/tours/${tour.imageCover}`],
+                        images:[`${req.protocol}://${req.get("host")}/img/tours/${tour.imageCover}`],
                     }
                 }
 
